3d4d4aa9ca7a6069018666f4f44a9aa886e614bb
Sourav Das <souravdas4816@gmail.com>
2022-08-26 22:47:41 +0530

    Add files via upload

---
+++ 6.1 - Functions (Reeborg's World Challange).py
@@ -0,0 +1,46 @@
+# All python build-in functions here> https://docs.python.org/3/library/functions.html
+# visit this website to understand below written code > https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
+# Turn right and turn around command ----
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def turn_around():
+    turn_left()
+    turn_left()
+move()
+turn_left()
+move()
+move()
+turn_right()
+move()
+move()
+turn_around()
+move()
+move()
+turn_left()
+move()
+move()
+turn_right()
+move()
+turn_around()
+# and robot get back to starting position.
+
+# now make a square and get back the robo to initaial position.
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def turn_around():
+    turn_left()
+    turn_left()
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_around()
