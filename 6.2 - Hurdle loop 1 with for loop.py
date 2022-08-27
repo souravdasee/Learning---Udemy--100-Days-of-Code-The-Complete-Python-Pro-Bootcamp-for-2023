5abcd1012f167852d4769d6605f0ab5f268faa10
Sourav Das <developer.souravdas.ee.com>
2022-08-27 22:04:41 +0530

    +

---
+++ 6.2 - Hurdle loop 1 with for loop.py
@@ -0,0 +1,20 @@
+# visit this website to run code > https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+# we have to stop at finishd mark
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+for step in range(6):
+    jump()
