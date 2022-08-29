585bfbfa475ed71d19090774a9b8e62dfa9dbb74
Sourav Das <developer.souravdas.ee.com>
2022-08-29 19:37:52 +0530

    +

--- 7.1 - Handman game  #1 Checking user's answer.py
+++ 7.1 - Handman game  #1 Checking user's answer.py
@@ -1,10 +1,11 @@
+#You might need to understand Python Lists > https://developers.google.com/edu/python/lists
+
 #Step 1 
 import random
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
-
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
 guess = input("Guess a letter: ").lower()
 
